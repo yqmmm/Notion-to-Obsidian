384633c457ebf09608ca119f8dfbb0b3c8a9f414
Qianmian Yu <im.qianmian.yu@gmail.com>
2020-05-29 18:07:08 +0800

    remove debug output

--- convert.py
+++ convert.py
@@ -11,13 +11,13 @@ class NotionConverter:
         match = self.uuidPattern.fullmatch(name)
         return None if match is None else match.group(1)
     
-    def replaceLink(self, content):
+    def replaceMdLink(self, content):
         return re.subn(self.notionLinkPattern, lambda match : f'[[{match.group(1)}]]', content)
 
     def renameAll(self, path):
         self.mapping = {}
         for root, dirs, files in os.walk(path, topdown=True):
-            newDirs = []
+            dirs.clear()
             for filename in files:
                 fullFilename = os.path.join(root, filename)
                 if not fullFilename.endswith(".md"): continue
@@ -31,27 +31,27 @@ class NotionConverter:
                 # Rename the directory associated with this md file
                 dir = fullFilename[:-3]
                 if os.path.isdir(dir):
-                    print(f"rename {dir} to {title}")
                     os.rename(dir, os.path.join(root, title))
-                    newDirs.append(title)
-            
-            dirs[:] = newDirs[:]
+                    dirs.append(title)
         
+    def replaceLinks(self, path):
+        numLinks = 0
         for root, dirs, files in os.walk(path):
             for filename in files:
                 fullFilename = os.path.join(root, filename)
                 if not fullFilename.endswith(".md"): continue
                 with open(fullFilename) as f:
                     content = f.read()
-                    (content, n) = self.replaceLink(content)
-                    if n > 0: print(n)
-                    # for key in self.mapping:
-                    #     content = content.replace(key, self.mapping[key])
+                    (content, n) = self.replaceMdLink(content)
+                    numLinks += n
                 with open(fullFilename, "w") as f:
                     f.write(content)
+        return numLinks
     
     def doTask(self, path):
         self.renameAll(path)
+        numLinks = self.replaceLinks(path)
+        print(f'Fixed {numLinks} links')
 
 if __name__ == "__main__":
     convert = NotionConverter()
